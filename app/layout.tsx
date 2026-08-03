6c22f2a2e9bcaeecb854452bc108ac5b1f3c7012
rashtrapal143 <rashtrapalw143@gmail.com>
2026-08-03 18:05:34 +0530

    added logo in meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,39 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter, Poppins } from 'next/font/google'
-import { Navigation } from '../src/app/components/Navigation'
-import { Footer } from '../src/app/components/Footer'
+  import './globals.css'
+  import type { Metadata } from 'next'
+  import { Inter, Poppins } from 'next/font/google'
+  import { Navigation } from '../src/app/components/Navigation'
+  import { Footer } from '../src/app/components/Footer'
 
 
-const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
-const poppins = Poppins({ 
-  weight: ['400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'], 
-  display: 'swap', 
-  variable: '--font-poppins' 
-})
+  const inter = Inter({ subsets: ['latin'], display: 'swap', variable: '--font-inter' })
+  const poppins = Poppins({ 
+    weight: ['400', '500', '600', '700', '800', '900'],
+    subsets: ['latin'], 
+    display: 'swap', 
+    variable: '--font-poppins' 
+  })
 
-export const metadata: Metadata = {
-  title: 'Apropos Drive',
-  description: 'Eco-friendly EV solutions company',
-  keywords: ['EV', 'Electric Vehicles', 'Electric Bikes', 'Eco Vehicles'],
+  export const metadata: Metadata = {
+    title: 'Apropos Drive',
+    description: 'Eco-friendly EV solutions company',
+    keywords: ['EV', 'Electric Vehicles', 'Electric Bikes', 'Eco Vehicles'],
+    icons: {
+      icon: '/photos/logo2.png',
+      shortcut: '/photos/logo.png',
+      apple: '/photos/logo.png',
+    },
 
-}
+  }
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.variable} ${poppins.variable} bg-[#F8FAFB]`}>
-        <Navigation />
-        {children}
+  export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <html lang="en">
+        <body className={`${inter.variable} ${poppins.variable} bg-[#F8FAFB]`}>
+          <Navigation />
+          {children}
 
-        <Footer />
-      </body>
-    </html>
-  )
-}
+          <Footer />
+        </body>
+      </html>
+    )
+  }

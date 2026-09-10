e856082bef1416a02a8619080689ecf051862921
rashtrapal143 <rashtrapalw143@gmail.com>
2026-09-10 10:43:57 +0530

    blog 3 added test 8

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,5 +4,3 @@ import { RouteSectionSkeleton } from '../src/app/components/RouteSectionSkeleton
 export default function Loading() {
   return <RouteSectionSkeleton className="min-h-screen pt-24" />
 }
-
-                  
